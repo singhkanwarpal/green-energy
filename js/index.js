6693a7b4c44d448174bda3dd5e32e0b29f4b491b
Kanwar Pal Singh <40355127+kanwarps@users.noreply.github.com>
2021-07-26 22:34:26 -0400

    Code

---
+++ js/index.js
@@ -0,0 +1,11 @@
+//Getting references
+
+
+//Resource
+const Data={
+    partial1: "This is content 1. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure odio quas numquam amet repudiandae accusantium reprehenderit et maxime, fugiat eveniet? Id sit corporis dolorum voluptatum aut quis. Accusantium, ipsa enim!",
+    partial2: "This is content 2.",
+    partial3: "This is content 3.",
+};
+
+console.log(Data.partial1);
