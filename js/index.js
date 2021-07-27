1e052af74cf7ce5f31396261d82961ce28071726
Kanwar Pal Singh <40355127+kanwarps@users.noreply.github.com>
2021-07-26 23:57:44 -0400

    Code

--- js/index.js
+++ js/index.js
@@ -4,17 +4,17 @@ let $dc = document.getElementById('dynamic-content')
 //Resource
 const Data={
     partial1: {
-        heading: 'Heading 1',
+        heading: 'Energy 1',
         imageURL: 'img/image1.jpg',
         content: 'This is content 1. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure odio quas numquam amet repudiandae accusantium reprehenderit et maxime, fugiat eveniet? Id sit corporis dolorum voluptatum aut quis. Accusantium, ipsa enim!'
     },
     partial2: {
-        heading: 'Heading 2',
+        heading: 'Energy 2',
         imageURL: 'img/image2.jpg',
         content: 'This is content 2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure odio quas numquam amet repudiandae accusantium reprehenderit et maxime, fugiat eveniet? Id sit corporis dolorum voluptatum aut quis. Accusantium, ipsa enim!'
     },
     partial3: {
-        heading: 'Heading 3',
+        heading: 'Energy 3',
         imageURL: 'img/image3.jpg',
         content: 'This is content 3. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure odio quas numquam amet repudiandae accusantium reprehenderit et maxime, fugiat eveniet? Id sit corporis dolorum voluptatum aut quis. Accusantium, ipsa enim!'
     }
